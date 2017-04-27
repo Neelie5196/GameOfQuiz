7b23729e4d5a748d5cada03dc8ec2551fe3740bb
Ting17 <its.me.ting4331451@gmail.com>
2017-04-27 17:13:29 +0800

    Remove wrong sql file uploaded
    Remove wrong sql fileuploaded

--- quiz.sql
+++ quiz.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 27, 2017 at 10:43 AM
+-- Generation Time: Apr 27, 2017 at 11:08 AM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -51,6 +51,28 @@ INSERT INTO `question` (`quizID`, `questionID`, `question`, `type`, `hints`, `in
 (1, 3, 'Best synonym for frustration', 'M', 'none', 'upset feeling', 'first feeling', 'successful feeling', 'cold feeling', 'A'),
 (2, 4, '___ he play baseball after school', 'B', 'Has / Does / Do / Is', 'Does', '', '', '', ''),
 (1, 5, 'Best synonym for employer', 'M', 'none', 'legal professional', 'waitress', 'boss', 'farmer', 'C');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE IF NOT EXISTS `quiz` (
+  `quizID` int(11) NOT NULL AUTO_INCREMENT,
+  `quizTopic` varchar(50) NOT NULL,
+  `bonus` varchar(1000) NOT NULL,
+  PRIMARY KEY (`quizID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`quizID`, `quizTopic`, `bonus`) VALUES
+(1, 'Volcabulary', 'You know more than you think. TRUST yourself. - Dr.Benjamin Mclane Spock'),
+(2, 'Grammar', 'The best proof of love is trust. - Dr.Joyce Brothers'),
+(3, 'AngularJS', 'To succeed in your mission you must have single-minded devotion to your goal. - A.P.J. Abdul Kalam');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
